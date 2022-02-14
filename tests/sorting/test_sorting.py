2bb3b385e9f663213fb302e994bd90cdb6c3c46d
Sarah <sarahgsb@hotmail.com>
2022-02-14 13:45:16 -0300

    requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,12 @@
-# from src.sorting import sort_by
+from src.jobs import read
+from src.insights import get_min_salary
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
+    job = read("src/jobs.csv")
+    sort_by(job, "min_salary")
+    sort = job[0]["min_salary"]
+    assert type(job) is list
+    assert sort == str(get_min_salary("src/jobs.csv"))
     pass
